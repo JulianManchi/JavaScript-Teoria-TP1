8d3f2f7883d9db01c2e805dbc20823ffbe0f387c
Julian Mancinelli <Julianomanchi03@gmail.com>
2022-05-29 18:05:39 -0300

    Agregamos conceptos de variables

---
+++ js/1-variables.js
@@ -0,0 +1,46 @@
+//Esto es un comentario simple
+
+/*Esto es un comentario
+de varias lineas*/
+
+//mostrar un comentario en la pantalla
+
+document.write("Hola Mundo");
+
+//mostrar un mensaje o comentario en la consola del navegador
+
+console.log("Hola Mundo");
+
+//mostrar un mensaje en una ventana emergente
+/*alert("Hola Comisión 2i");*/
+
+//Crear, declarar, inicializar Variables (var{quedó de desuso} - let - const)
+
+let persona = 'Nico Andrade';
+var persona2 = 'Daniela';
+
+document.write("<br>Persona: " + persona);
+document.write("<br>Segunda persona: " + persona2);
+
+//Cambiar el valor de una variable
+
+persona = 'Francisco';
+document.write('<br>Persona: ' + persona);
+
+//Cuando uso la constante no puedo modificar el valor, es un valor fijo
+
+const url = 'http://127.0.0.1:5500/index.html';
+document.write('<br>URL: ' + url);
+
+//variables con números
+
+let anio = 1997;
+document.write('<br>Año de Nacimiento: ' + anio);
+
+//Variables buleanas {true - false}
+
+let foco = false;
+
+foco = true;
+
+document.write('<br>Foco: ' + foco);
