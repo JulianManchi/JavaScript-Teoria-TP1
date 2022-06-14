4ef653026825052b1ab46e6c3d959f5e972d5482
Julian Mancinelli <Julianomanchi03@gmail.com>
2022-06-14 11:28:22 -0300

    Agregamos, eliminamos y modificamos elementos en el arreglo

--- js/5-Arreglos.js
+++ js/5-Arreglos.js
@@ -1,6 +1,6 @@
 // Ejemplo: Crear una variable y poder modificarla
 
-// declarar o crear un arreglo
+// Declarar o crear un arreglo
 let usuarios = [];
 
 let series = ['Naruto', 2, 'Game of Trons', 8, 'Rick and Morty', 4];
@@ -15,4 +15,42 @@ console.log(series.length);
 // Bucle para mostrar todos los elementos del arreglo
 for(let posicion = 0; posicion < series.length; posicion = posicion + 1){
     document.write('<br>Elemento: ' + series[posicion]);
-}
+};
+
+// Cuando accedemos a elementos que no existen en el arreglo
+document.write('<br>' + series[10]);
+
+// Agregar un elemento al final del arreglo
+series.push('Ozark');
+
+// Bucle para mostrar todos los elementos del arreglo
+for(let posicion = 0; posicion < series.length; posicion = posicion + 1){
+    document.write('<br>Elemento: ' + series[posicion]);
+};
+
+// Agregar un elemento en una posicion particular del arreglo ya creado
+series.splice(3, 0, 'Breaking Bad');
+
+document.write('<hr>');
+
+for(let posicion = 0; posicion < series.length; posicion = posicion + 1){
+    document.write('<br>Elemento: ' + series[posicion]);
+};
+
+// Eliminar elementos desde una posicion particular
+// series.splice(5, 2); //Ej: (posicion del elemento, catidad de elementos a eliminar);
+series.splice(7); // Elimina los elementos de la derecha desde la posicion 5
+document.write('<hr>');
+
+for(let posicion = 0; posicion < series.length; posicion = posicion + 1){
+    document.write('<br>Elemento: ' + series[posicion]);
+};
+
+// Modificar un elemento del arreglo
+series[6] = true;
+
+document.write('<hr>');
+
+for(let posicion = 0; posicion < series.length; posicion = posicion + 1){
+    document.write('<br>Elemento: ' + series[posicion]);
+};
