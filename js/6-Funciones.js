22bd91a118a4656a398d6e16d381e44c39fff15a
Julian Mancinelli <Julianomanchi03@gmail.com>
2022-06-21 16:55:42 -0300

    Agregamos arrays functions

--- js/6-Funciones.js
+++ js/6-Funciones.js
@@ -8,23 +8,38 @@ function saludar(){
 saludar();
 
 // Funcion que retorna valor
-function convertirDolarAPesos(dolares){
+// function convertirDolarAPesos(dolares){
+//   let pesosFinales = dolares * 210;
+//   return pesosFinales;
+//   return se usa para poner una variable que se va a extraer
+//}
+
+
+
+// ES6 arrow functions
+const convertirDolarAPesos = (dolares) =>{
     let pesosFinales = dolares * 210;
+    console.log(pesosFinales);
     return pesosFinales;
-    // return se usa para poner una variable que se va a extraer
-};
+}
 
 // funcion con parametros
 function saludarPersona(nombrerecibido, apellidorecibido){
     document.write(`<br> Binevenido ${nombrerecibido} ${apellidorecibido}`);
 
-};
+}
 
 let nombre = prompt('Ingrese un nombre');
 let apellido = prompt('Ingrese un apellido');
+let nombre1 = 'Jony'
 
 saludarPersona(nombre, apellido);
+saludarPersona('Daniela', 'Cardozo');
+saludarPersona(nombre1, 'Cardozo')
 
 let pesosSinImpuestos = convertirDolarAPesos(60);
 
 document.write(` <br> Juego en pesos $ ${pesosSinImpuestos}`);
+document.write(`<br> Juego en pesos $ ${convertirDolarAPesos(10)}`);
+
+convertirDolarAPesos(120);
